3002bb0cb09c3402dd5358fd8e9be00db673ff43
Colby M. White <colbywhite@users.noreply.github.com>
2021-08-22 15:26:45 -0500

    feat: bring back validators

--- src/app/create-quote/create-quote-form.component.ts
+++ src/app/create-quote/create-quote-form.component.ts
@@ -1,12 +1,17 @@
 import { Component, ElementRef, EventEmitter, Output } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Quote } from '../quotes/quotes.service';
 
 @Component({
   selector: 'sadf-create-quote-form',
-  styles: [
-    'input.ng-invalid.ng-touched, textarea.ng-invalid.ng-touched { border: 1px solid indianred}'
-  ],
+  styles: [`
+    input.ng-invalid.ng-touched,
+    input.ng-invalid.ng-dirty,
+    textarea.ng-invalid.ng-touched,
+    textarea.ng-invalid.ng-dirty {
+      border: 1px solid indianred
+    }
+  `],
   template: `
     <h1>Add a quote</h1>
     <form [formGroup]="quoteForm" (ngSubmit)="submit()">
@@ -14,19 +19,19 @@ import { Quote } from '../quotes/quotes.service';
       <input type="text" formControlName="artist" id="artist"/>
       <div *ngIf="artist.invalid && (artist.dirty || artist.touched)">
         <div *ngIf="artist.errors?.required">Artist is required.</div>
-        <div *ngIf="artist.errors?.maxlength">Artist must be less than 100 characters long.</div>
+        <div *ngIf="artist.errors?.maxlength">Artist must be less than 25 characters long.</div>
       </div>
       <label for="source">Source</label>
       <input type="text" formControlName="source" id="source"/>
       <div *ngIf="source.invalid && (source.dirty || source.touched)">
         <div *ngIf="source.errors?.required">Source is required.</div>
-        <div *ngIf="source.errors?.maxlength">Source must be less than 100 characters long.</div>
+        <div *ngIf="source.errors?.maxlength">Source must be less than 25 characters long.</div>
       </div>
       <label for="quote">Quote</label>
       <textarea formControlName="quote" id="quote"></textarea>
       <div *ngIf="quote.invalid && (quote.dirty || quote.touched)">
         <div *ngIf="quote.errors?.required">Quote is required.</div>
-        <div *ngIf="quote.errors?.maxlength">Quote must be less than 300 characters long.</div>
+        <div *ngIf="quote.errors?.maxlength">Quote must be less than 100 characters long.</div>
       </div>
       <button type="submit">Submit</button>
       <button (click)="cancel()">Cancel</button>
@@ -35,9 +40,9 @@ import { Quote } from '../quotes/quotes.service';
 })
 export class CreateQuoteFormComponent {
   public quoteForm: FormGroup = this.formBuilder.group({
-    artist: ['', []],
-    source: ['', []],
-    quote: ['', []]
+    artist: ['', [Validators.required, Validators.maxLength(25)]],
+    source: ['', [Validators.required, Validators.maxLength(25)]],
+    quote: ['', [Validators.required, Validators.maxLength(100)]]
   });
 
   @Output()
@@ -49,7 +54,6 @@ export class CreateQuoteFormComponent {
   }
 
   public submit() {
-    console.log('submit', this.quoteForm.value);
     if (this.quoteForm.valid) {
       this.formSubmitted.emit(this.quoteForm.value);
     }
