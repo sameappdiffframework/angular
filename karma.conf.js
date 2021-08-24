7c789d6914a161b9bc9d932ef9315b4d84089de0
Colby M. White <colbywhite@users.noreply.github.com>
2021-08-24 18:32:41 -0500

    refactor: change name of project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/sadf-angular'),
+      dir: require('path').join(__dirname, './coverage/rap-quotes-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
