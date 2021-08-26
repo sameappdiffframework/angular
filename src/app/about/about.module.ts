ad2ac7a44ed3e936f81efce9737fcffa39c2dcfb
Colby M. White <colbywhite@users.noreply.github.com>
2021-08-26 14:50:32 -0500

    rewrite: add about route

---
+++ src/app/about/about.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { AboutComponent } from './about.component';
+
+@NgModule({
+  declarations: [AboutComponent],
+  exports: [AboutComponent]
+})
+export class AboutModule {
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AboutModule } from './about/about.module';
 import { HomeComponent } from './home/home.component';
 import { HomeModule } from './home/home.module';
 
 const routes: Routes = [
+  { path: 'about', component: AboutComponent},
   { path: '', component: HomeComponent}
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    HomeModule
+    HomeModule,
+    AboutModule
   ],
   exports: [RouterModule]
 })
